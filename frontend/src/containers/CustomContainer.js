0330f3984eb0b2f6c4beaf0599fc90b5df8d8b79
unknown <crozeh56@gmail.com>
2020-02-14 08:20:07 +0900

    이름으로 검색완료

---
+++ frontend/src/containers/CustomContainer.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+
+class CustomCotainer extends Component{
+    render() {
+        return(
+            <div> 똑똑한놈 </div>
+        );
+    }
+}
+
+export default CustomCotainer;
